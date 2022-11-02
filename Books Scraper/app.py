d412aec1118d11462d8e44aa0ae6f53ee968e533
AatmikDahal <108218372+AatmikDahal@users.noreply.github.com>
2022-11-03 02:52:54 +0545

    Delete Books Scraper directory

--- Books Scraper/app.py
+++
@@ -1,9 +0,0 @@
-from cgi import print_arguments
-from parser.html_parser import  BooksParser 
-from locator.locator import  BooksPageLocator
-
-_books = [] #['{self.title} for {self.price} with {self.rating} star rating \n']
-
-parsed = BooksParser(BooksPageLocator.page)
-_books.extend(parsed.parsing)
-
